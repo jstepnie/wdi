16d2f89f87ba4865d1c14edfdcd0cc87144d7b99
jstepnie <jstepnie@student.agh.edu.pl>
2021-10-26 17:40:43 +0200

    did 4th task

---
+++ laboratorium_3/zadanie_4.py
@@ -0,0 +1,20 @@
+name = "Jan"
+surname = "Stępniewski"
+favpet = "wilk"
+favfood = "pizza"
+a = 5
+b = 7
+print(name + " " + surname)
+print(favpet)
+print(favfood)
+print(round(a / b, 1))
+print(round(a / b, 3))
+print(round(a / b, 5))
+print(round(a / b, 10))
+
+print("""Jan 
+Stępniewski
+ulubione zwierzę: wilk
+ulubiona potrawa: pizza
+wynik dzielenia 5 przez 7 = 0.7""")
+
