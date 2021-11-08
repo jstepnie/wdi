23af66f7082aa41a72dfb1ce9eb2692a3e82a86a
jstepnie <jstepnie@student.agh.edu.pl>
2021-11-08 23:37:21 +0100

    did 45th task

--- laboratorium_5/zadanie_4.py
+++ laboratorium_5/zadanie_4.py
@@ -1,20 +1,20 @@
 print("rozwiązanie metodą bisekcji równania x^2 - 2 = 0")
-delta = 0.00001
+approx = 0.00001
 element_1 = 1
 element_2 = 20
 tries = 100
 
 def f(x):
-    "dfinicja funkcji: "
+    "definicja funkcji: "
     # print( (x ** 2) - 2)
 
     return (x ** 2) - 2
 
-def bisection(tries, delta, function, element_1, element_2):
+def bisection(tries, approx, function, element_1, element_2):
     for k in range(1, tries):
         mean = (element_1 + element_2) / 2
 
-        if abs(function(mean)) < delta:
+        if abs(function(mean)) < approx:
             return mean
         elif function(mean) * function(element_1) < 0:
             element_2 = mean
@@ -23,6 +23,6 @@ def bisection(tries, delta, function, element_1, element_2):
     return None
 
 
-m_zerowe = bisection(tries, delta, f, element_1, element_2)
+m_zerowe = bisection(tries, approx, f, element_1, element_2)
 print(m_zerowe)
 print("drugie miejsce zerowe: ", - m_zerowe)
