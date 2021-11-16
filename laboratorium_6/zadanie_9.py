7b8001172c41f030ebf2053a7045482d703cbf4e
jstepnie <jstepnie@student.agh.edu.pl>
2021-11-16 12:03:17 +0100

    did 9th task

--- laboratorium_6/zadanie_9.py
+++ laboratorium_6/zadanie_9.py
@@ -2,33 +2,35 @@
 number = int(input("Podajliczbę naturalną: "))
 x = 1
 number_2 = number // 2
-list = []
+list_m = []
+list_sub = []
 while True:
     if number <= 0:
         print("liczba ma być naturalna i nie być zerem")
         break
     else:
         for i in range(1, number_2 + 1):
-            if number % i == 0:
-                print("fajnie",i)
+
+            if (number % i == 0) and list_m.count(i) == 0:
+                # print("ff", i)
                 x = i
-                list = list + [x]
-                print(list)
+                list_m = list_m + [x]
                 y = number / x
-                if abs(x - y) < number - 1:
-                    multiplication = x * y
-                    print(y, "*", x)
-                elif abs(x - y) <
-                else:
-                    multiplication = number * 1
-                    print("rozkład na iloczyn o najmniejszej różnicy to: ", number, "*", 1)
+                list_m = list_m + [y]
+                list_sub = list_sub + [abs(x - y)]
 
             else:
                 x = x
-                list = list
+                list_m = list_m
                 y = number / x
-
-
+        # print(list_m)
+        # print(list_sub)
+        for d in list_m:
+            if min(list_sub) == abs(d - (number / d)):
+                print("iloczyn, o najmniejszej różnicy składników, tworzący tą liczbę: ")
+                print(number, "=", d, "*", (number / d))
+            else:
+                print(".")
         break
 
 
