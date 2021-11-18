b21f27368e617b9ee8227a1d79f2c6d5fc1faab8
jstepnie <jstepnie@student.agh.edu.pl>
2021-11-18 12:57:45 +0100

    added exceptions

--- laboratorium_6/zadanie_9.py
+++ laboratorium_6/zadanie_9.py
@@ -1,19 +1,24 @@
 print("Program rozkładający liczbę naturalną na iloczyn dwóch liczb takich, że różnica między nimi jest najmniejsza")
 while True:
-    number = int(input("Podajliczbę naturalną: "))
+    number = float(input("Podaj liczbę naturalną: "))
     x = 1
     number_2 = number // 2
-    list_m = []
-    list_sub = []
+    list_m = []  #lista podzielników
+    list_sub = []  #lista różnic
     while True:
-        if number <= 0:
-            print("liczba ma być naturalna i nie być zerem")
+        if number <= 1:
+            print("liczba ma być naturalna i nie być zerem, ani jedynką")
+            break
+        elif number % 1 != 0:
+            print("liczba musi być naturalna!")
             break
         else:
-            for i in range(1, number_2 + 1):
+            int(number)
+            int(number_2)
+            for i in range(1, int(number_2 + 1)):
 
                 if (number % i == 0) and list_m.count(i) == 0:
-                    # print("dzielnik liczby to: ", i)
+                    print("dzielnik liczby to: ", i)
                     x = i
                     list_m = list_m + [x]
                     y = number // x
@@ -29,9 +34,12 @@ while True:
                 if min(list_sub) == (d - (number / d)):
                     print("iloczyn, o najmniejszej różnicy składników, tworzący tą liczbę: ")
                     print(number, "=", d, "*", (number / d))
+                    # print("zaczynamy na nowo:")
                 else:
                     list_m = list_m
             break
+
+#przykłady testowe - 2342938472 = 41838187.0 * 56, -5 - nie zwróci,
 
 
 
