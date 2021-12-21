a384a58482732e469752ac04e95e5d8a36e0dda2
jstepnie <jstepnie@student.agh.edu.pl>
2021-12-21 16:10:22 +0100

    did task 8

--- laboratorium_10/zadanie_8.py
+++ laboratorium_10/zadanie_8.py
@@ -4,60 +4,93 @@ filepathB = "plikB.txt"
 filepathC = "plikC.txt"
 def pobranie_danych(filepath):
     f = open(filepath, "r", encoding="utf-8")
-    words = f.readlines()
-    print(words)
-    return words
+    words = f.read().splitlines()
+    elements = []
+    for element in words:
+        element1 = element.split()
+        elements.append(element1)
+    print(elements)
+    return elements
 
 def repetition(wordsA, wordsB):
     repeat = []
-    for element in wordsA:
-        if wordsB.count(element) != 0:
-            repeat.append(element)
+    for y1 in wordsA:
+        for x1 in y1:
+            for y2 in wordsB:
+                if y2.count(x1) != 0 and repeat.count(x1) == 0:
+                    repeat.append(x1)
     print(repeat)
     return repeat
 
 def addition(wordsA, wordsB, repetition, filepathC):
-    wordsA1 = wordsA[:]
-    wordsB1 = wordsB[:]
+    wordsA1 = []
+    wordsB1 = []
     repetition1 = repetition[:]
+    for y1 in wordsA:
+        for x1 in y1:
+            wordsA1.append(x1)
+    for y2 in wordsB:
+        for x2 in y2:
+            wordsB1.append(x2)
     for element in repetition:
-        if wordsA.count(element) != 0:
-            wordsA1.pop(wordsA1.index(element))
-        if wordsB.count(element) != 0:
-            wordsB1.pop(wordsB1.index(element))
-    wordsA1 = str(wordsA1).replace("[", "").replace("]", "").replace(",", " ").replace("'", "")
-    wordsB1 = str(wordsB1).replace("[", "").replace("]", "").replace(",", " ").replace("'", "")
-    repetition1 = str(repetition1).replace("[", "").replace("]", "").replace(",", " ").replace("'", "")
+        if wordsA1.count(element) != 0:
+            wordsA1.remove(element)
+        if wordsB1.count(element) != 0:
+            wordsB1.remove(element)
     plikC = open(filepathC, "a")
-    plikC.write(wordsA1)
-    plikC.write("\n")
-    plikC.write(wordsB1)
-    plikC.write("\n")
-    plikC.write(repetition1)
+    n = 0
+    a = 0
+    b = 7
+    for elementa in wordsA1:
+            if n < 7:
+                plikC.write(str(elementa))
+                plikC.write(" ")
+                n = n + 1
+            if n == 7:
+                n = 0
+                plikC.write("\n")
+                plikC.write(str(wordsB1[a:b]).replace("[", "").replace("]", "").replace(",", " ").replace("'", ""))
+                plikC.write("\n")
+                a = a + 7
+                b = b + 7
     plikC.close()
 
 def substraction(wordsA, repetition, filepathC):
-    wordsA1 = wordsA[:]
-    repetition1 = repetition[:]
+    wordsA1 = []
+    n = 0
+    for y1 in wordsA:
+        for x1 in y1:
+            wordsA1.append(x1)
+    repetition = repetition[:]
     for element in repetition:
-        if wordsA.count(element) != 0:
+        if wordsA1.count(element) != 0:
             wordsA1.pop(wordsA1.index(element))
-    wordsA1 = str(wordsA1).replace("[", "").replace("]", "").replace(",", " ").replace("'", "")
     plikC = open(filepathC, "a")
-    plikC.write(wordsA1)
+    for elementa in wordsA1:
+            if n < 7:
+                plikC.write(str(elementa))
+                plikC.write(" ")
+                n = n + 1
+            if n == 7:
+                n = 0
+                plikC.write("\n")
     plikC.close()
 
 def multiplication(repetition, filepathC):
     repetition1 = repetition[:]
+    repetition1.sort()
     for element in repetition1:
         plikC = open(filepathC, "a")
-        repetition2 = str(element).replace("[", "").replace("]", "").replace(",", " ").replace("'", "")
-        plikC.write(repetition2)
+        for i in range(7):
+            repetition2 = str(element).replace("[", "").replace("]", "").replace(",", " ").replace("'", "")
+            plikC.write(repetition2)
+            plikC.write(" ")
+        plikC.write("\n")
         plikC.close()
 
 pobranieA = pobranie_danych(filepathA)
 pobranieB = pobranie_danych(filepathB)
 repetitionU = repetition(pobranieA, pobranieB)
-# addition(pobranieA, pobranieB, repetitionU, filepathC)
+addition(pobranieA, pobranieB, repetitionU, filepathC)
 # substraction(pobranieA, repetitionU, filepathC)
 # multiplication(repetitionU, filepathC)
